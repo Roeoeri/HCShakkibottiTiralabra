570c97cf5dbc1a1c647e8969ded9020b3989d609
Roeoeri <tomi.haapakoski@helsinki.fi>
2020-02-01 00:52:55 +0200

    koodia siistitty

--- src/main/java/chess/bot/TiraBot.java
+++ src/main/java/chess/bot/TiraBot.java
@@ -12,49 +12,42 @@ import chess.rules.Piece;
  *
  * @author htomi
  */
-public class TiraBot implements ChessBot{
-    
+public class TiraBot implements ChessBot {
+
     public Piece[][] board;
     public Piece[] blackPawns;
-  
-    
-    public TiraBot(){
-        
+
+    public TiraBot() {
+
         board = new Piece[][]{
-            {Piece.WROOK,Piece.WKNIGHT,Piece.WBISHOP,Piece.WQUEEN,Piece.WKING,Piece.WBISHOP,Piece.WKNIGHT,Piece.WROOK},
-            {Piece.WPAWN,Piece.WPAWN,Piece.WPAWN,Piece.WPAWN,Piece.WPAWN,Piece.WPAWN,Piece.WPAWN,Piece.WPAWN},
-            {Piece.EMPTY,Piece.EMPTY,Piece.EMPTY,Piece.EMPTY,Piece.EMPTY,Piece.EMPTY,Piece.EMPTY,Piece.EMPTY},
-            {Piece.EMPTY,Piece.EMPTY,Piece.EMPTY,Piece.EMPTY,Piece.EMPTY,Piece.EMPTY,Piece.EMPTY,Piece.EMPTY},
-            {Piece.EMPTY,Piece.EMPTY,Piece.EMPTY,Piece.EMPTY,Piece.EMPTY,Piece.EMPTY,Piece.EMPTY,Piece.EMPTY},
-            {Piece.BPAWN,Piece.BPAWN,Piece.BPAWN,Piece.BPAWN,Piece.BPAWN,Piece.BPAWN,Piece.BPAWN,Piece.BPAWN},
-            {Piece.BROOK,Piece.BKNIGHT,Piece.BBISHOP,Piece.BQUEEN,Piece.BKING,Piece.BBISHOP,Piece.BKNIGHT,Piece.BROOK}      
-        };  
+            {Piece.WROOK, Piece.WKNIGHT, Piece.WBISHOP, Piece.WQUEEN, Piece.WKING, Piece.WBISHOP, Piece.WKNIGHT, Piece.WROOK},
+            {Piece.WPAWN, Piece.WPAWN, Piece.WPAWN, Piece.WPAWN, Piece.WPAWN, Piece.WPAWN, Piece.WPAWN, Piece.WPAWN},
+            {Piece.EMPTY, Piece.EMPTY, Piece.EMPTY, Piece.EMPTY, Piece.EMPTY, Piece.EMPTY, Piece.EMPTY, Piece.EMPTY},
+            {Piece.EMPTY, Piece.EMPTY, Piece.EMPTY, Piece.EMPTY, Piece.EMPTY, Piece.EMPTY, Piece.EMPTY, Piece.EMPTY},
+            {Piece.EMPTY, Piece.EMPTY, Piece.EMPTY, Piece.EMPTY, Piece.EMPTY, Piece.EMPTY, Piece.EMPTY, Piece.EMPTY},
+            {Piece.BPAWN, Piece.BPAWN, Piece.BPAWN, Piece.BPAWN, Piece.BPAWN, Piece.BPAWN, Piece.BPAWN, Piece.BPAWN},
+            {Piece.BROOK, Piece.BKNIGHT, Piece.BBISHOP, Piece.BQUEEN, Piece.BKING, Piece.BBISHOP, Piece.BKNIGHT, Piece.BROOK}
+        };
     }
-    
-    
-    
-    
+
     @Override
-    public String nextMove(GameState gs){
-        return"e1e2";
+    public String nextMove(GameState gs) {
+        return "e1e2";
     }
-    
-    
-    public void updateBoard(String move){
-        
-        int x1 = move.charAt(0)-97;
-        int y1 = move.charAt(1)-49;
-        
-        int x2 = move.charAt(2)-97;
-        int y2 = move.charAt(3)-49;
-        
-  
+
+    public void updateBoard(String move) {
+
+        int x1 = move.charAt(0) - 97;
+        int y1 = move.charAt(1) - 49;
+
+        int x2 = move.charAt(2) - 97;
+        int y2 = move.charAt(3) - 49;
+
         Piece PieceToBeMoved = board[y1][x1];
-        
-        
+
         board[y1][x1] = Piece.EMPTY;
         board[y2][x2] = PieceToBeMoved;
-             
+
     }
-    
+
 }
--- src/main/java/chess/rules/MovementChecker.java
+++ src/main/java/chess/rules/MovementChecker.java
@@ -13,235 +13,233 @@ import java.util.ArrayList;
  */
 public class MovementChecker {
 
-	private ArrayList<Piece[][]> legalMoves;
-	private final int size = 8;
-	private final int min = 0;
+    private ArrayList<Piece[][]> legalMoves;
+    private final int size = 8;
+    private final int min = 0;
 
-	public ArrayList<Piece[][]> getLegalMoves(Piece[][] board, boolean isWhite) {
-		legalMoves = new ArrayList();
-		if (isWhite) {
-			getLegalWhiteMoves(board);
-		} else {
-			getLegalBlackMoves(board);
-		}
-		return legalMoves;
-	}
+    public ArrayList<Piece[][]> getLegalMoves(Piece[][] board, boolean isWhite) {
+        legalMoves = new ArrayList();
+        if (isWhite) {
+            getLegalWhiteMoves(board);
+        } else {
+            getLegalBlackMoves(board);
+        }
+        return legalMoves;
+    }
 
-	private void getLegalWhiteMoves(Piece[][] board) {
-		for (int i = 0; i < size; i++) {
-			for (int j = 0; j < size; j++) {
-				Piece currentSquare = board[i][j];
-				switch (currentSquare) {
+    private void getLegalWhiteMoves(Piece[][] board) {
+        for (int i = 0; i < size; i++) {
+            for (int j = 0; j < size; j++) {
+                Piece currentSquare = board[i][j];
+                switch (currentSquare) {
 
-				case WPAWN:
-					getLegalPawnMoves(board, j, i);
-					break;
+                    case WPAWN:
+                        getLegalPawnMoves(board, j, i);
+                        break;
 
-				case WROOK:
-					getLegalRookMoves(board, j, i);
-					break;
-					
-				case WBISHOP:
-					getLegalBishopMoves(board,j,i);
-					break;
-					
-				case WQUEEN:
-					getLegalQueenMoves(board,j,i);
-					break;
-					
-				case WKING:
-					getLegalKingMoves(board,j,i);
-					break;
+                    case WROOK:
+                        getLegalRookMoves(board, j, i);
+                        break;
 
-				}
-				
-			}
-		}
+                    case WBISHOP:
+                        getLegalBishopMoves(board, j, i);
+                        break;
 
-	}
+                    case WQUEEN:
+                        getLegalQueenMoves(board, j, i);
+                        break;
 
-	private void getLegalBlackMoves(Piece[][] board) {
-		for (int i = 0; i < size; i++) {
-			for (int j = 0; j < size; j++) {
-				Piece currentSquare = board[i][j];
-				switch (currentSquare) {
+                    case WKING:
+                        getLegalKingMoves(board, j, i);
+                        break;
 
-				case BPAWN:
-					getLegalPawnMoves(board, j, i);
-					break;
+                }
 
-				case BROOK:
-					getLegalRookMoves(board, j, i);
-					break;
-					
-				case BBISHOP:
-					getLegalBishopMoves(board,j,i);
-					break;
-					
-				case BQUEEN:
-					getLegalQueenMoves(board,j,i);
-					break;
-					
-				case BKING:
-					getLegalKingMoves(board,j,i);
-				}
-				
-			}
-		}
-	}
+            }
+        }
 
-	private void getLegalPawnMoves(Piece[][] board, int x, int y) {
-		Piece ownColor = board[y][x];
-		Piece oppositeColor = getOppositeColor(ownColor);
-		
-		int pawnStep = 1;
-		if (board[y][x] == Piece.BPAWN) {
-			pawnStep = -1;
-		}
+    }
 
-		int yUp = y + pawnStep;
-		if (yUp < size) {
-			Piece contentOfTile = board[yUp][x];
-			if (contentOfTile == Piece.EMPTY) {
-				Piece[][] moveUp = moveCharacter(board, x, y, x, yUp);
-				legalMoves.add(moveUp);
-			}
-			int xRight = x + 1;
-			if (xRight < size) {
-				contentOfTile = board[yUp][xRight];
-				{
-					if (getColor(contentOfTile) == oppositeColor) {
-						Piece[][] attackRight = moveCharacter(board, x, y, xRight, yUp);
-						legalMoves.add(attackRight);
-					}
-				}
-			}
-			int xLeft = x - 1;
-			if (xLeft >= min) {
-				contentOfTile = board[yUp][xLeft];
-				{
-					if (getColor(contentOfTile) == oppositeColor) {
-						Piece[][] attackLeft = moveCharacter(board, x, y, xLeft, yUp);
-						legalMoves.add(attackLeft);
-					}
-				}
+    private void getLegalBlackMoves(Piece[][] board) {
+        for (int i = 0; i < size; i++) {
+            for (int j = 0; j < size; j++) {
+                Piece currentSquare = board[i][j];
+                switch (currentSquare) {
 
-			}
-		}
+                    case BPAWN:
+                        getLegalPawnMoves(board, j, i);
+                        break;
 
-	}
+                    case BROOK:
+                        getLegalRookMoves(board, j, i);
+                        break;
 
-	private void getLegalRookMoves(Piece[][] board, int x, int y) {
-		
-		Piece ownColor = getColor(board[y][x]);
-		
-		traverseUntilEnd(board,x,y,x+1,y,1,0,ownColor);
-		
-		traverseUntilEnd(board,x,y,x-1,y,-1,0,ownColor);
-		
-		traverseUntilEnd(board,x,y,x,y+1,0,1,ownColor);
-		
-		traverseUntilEnd(board,x,y,x,y-1,0,-1,ownColor);
-	}
-	
-	private void getLegalBishopMoves(Piece[][] board, int x, int y) {
-		
-		Piece ownColor = getColor(board[y][x]);
-		
-		traverseUntilEnd(board,x,y,x-1,y-1,-1,-1,ownColor);
-		
-		traverseUntilEnd(board,x,y,x+1,y-1,1,-1,ownColor);
-		
-		traverseUntilEnd(board,x,y,x-1,y+1,-1,1,ownColor);
-		
-		traverseUntilEnd(board,x,y,x+1,y+1,1,1,ownColor);
-	}
-	
-	private void getLegalQueenMoves(Piece[][] board, int x, int y) {
-		
-		getLegalRookMoves(board,x,y);
-		getLegalBishopMoves(board,x,y);
-	}
-	
-	private void getLegalKingMoves(Piece[][] board, int x, int y) {
-		
-		Piece ownColor = getColor(board[y][x]);
-		
-		for(int y2 = y-1; y2 <= y+1; y2++) {
-			for(int x2 = x-1; x2<=x+1; x2++) {
-				if(x2 >= size || x2<min || y2 >= size || y2 < min) {
-					continue;
-				}
-				Piece destinationTileColor = getColor(board[y2][x2]);
-				if(destinationTileColor == getOppositeColor(ownColor) || destinationTileColor == Piece.EMPTY) {
-					Piece[][] move = moveCharacter(board,x,y,x2,y2);
-					legalMoves.add(move);
-				}
-			}
-		}
-	}
-	
-	
-	
-	private void traverseUntilEnd(Piece[][] board,int startX, int startY, int x, int y, int xSteps, int ySteps, Piece ownColor) {
-		if(x >= size || x<min || y >= size || y < min) {
-			return;
-		}
-		
-		Piece tileColor = getColor(board[y][x]);
-		
-		if(tileColor == ownColor) {
-			return;
-		}
-		
-		if(tileColor == getOppositeColor(ownColor)) {
-			Piece[][] attack = moveCharacter(board,startX,startY,x,y);
-			legalMoves.add(attack);
-			return;
-		}
-		
-		Piece[][] moveToThisTile = moveCharacter(board,startX,startY,x,y);
-		legalMoves.add(moveToThisTile);
-		
-		traverseUntilEnd(board,startX,startY, x + xSteps, y+ySteps, xSteps, ySteps, ownColor);
-	}
-	
-	private Piece[][] moveCharacter(Piece[][] board, int x1, int y1, int x2, int y2) {
+                    case BBISHOP:
+                        getLegalBishopMoves(board, j, i);
+                        break;
 
-		Piece[][] boardToBeReturned = new Piece[8][8];
+                    case BQUEEN:
+                        getLegalQueenMoves(board, j, i);
+                        break;
 
-		for (int i = 0; i < size; i++) {
-			for (int j = 0; j < size; j++) {
-				boardToBeReturned[i][j] = board[i][j];
-			}
-		}
+                    case BKING:
+                        getLegalKingMoves(board, j, i);
+                }
 
-		Piece pieceAtCurrentLocation = board[y1][x1];
-		boardToBeReturned[y2][x2] = pieceAtCurrentLocation;
-		boardToBeReturned[y1][x1] = Piece.EMPTY;
+            }
+        }
+    }
 
-		return boardToBeReturned;
-	}
+    private void getLegalPawnMoves(Piece[][] board, int x, int y) {
+        Piece ownColor = board[y][x];
+        Piece oppositeColor = getOppositeColor(ownColor);
 
-	public Piece getColor(Piece piece) {
-		if (piece.toString().charAt(0) == 'B') {
-			return Piece.BLACK;
-		}
-		if (piece.toString().charAt(0) == 'W') {
-			return Piece.WHITE;
-		}
-		return Piece.EMPTY;
+        int pawnStep = 1;
+        if (board[y][x] == Piece.BPAWN) {
+            pawnStep = -1;
+        }
 
-	}
-	
-	public Piece getOppositeColor(Piece piece) {
-		if(getColor(piece)== Piece.WHITE) {
-			return Piece.BLACK;
-		}
-		if(getColor(piece)==Piece.BLACK) {
-			return Piece.WHITE;
-		}
-		return Piece.EMPTY;
-	}
+        int yUp = y + pawnStep;
+        if (yUp < size) {
+            Piece contentOfTile = board[yUp][x];
+            if (contentOfTile == Piece.EMPTY) {
+                Piece[][] moveUp = moveCharacter(board, x, y, x, yUp);
+                legalMoves.add(moveUp);
+            }
+            int xRight = x + 1;
+            if (xRight < size) {
+                contentOfTile = board[yUp][xRight];
+                {
+                    if (getColor(contentOfTile) == oppositeColor) {
+                        Piece[][] attackRight = moveCharacter(board, x, y, xRight, yUp);
+                        legalMoves.add(attackRight);
+                    }
+                }
+            }
+            int xLeft = x - 1;
+            if (xLeft >= min) {
+                contentOfTile = board[yUp][xLeft];
+                {
+                    if (getColor(contentOfTile) == oppositeColor) {
+                        Piece[][] attackLeft = moveCharacter(board, x, y, xLeft, yUp);
+                        legalMoves.add(attackLeft);
+                    }
+                }
+
+            }
+        }
+
+    }
+
+    private void getLegalRookMoves(Piece[][] board, int x, int y) {
+
+        Piece ownColor = getColor(board[y][x]);
+
+        traverseUntilEnd(board, x, y, x + 1, y, 1, 0, ownColor);
+
+        traverseUntilEnd(board, x, y, x - 1, y, -1, 0, ownColor);
+
+        traverseUntilEnd(board, x, y, x, y + 1, 0, 1, ownColor);
+
+        traverseUntilEnd(board, x, y, x, y - 1, 0, -1, ownColor);
+    }
+
+    private void getLegalBishopMoves(Piece[][] board, int x, int y) {
+
+        Piece ownColor = getColor(board[y][x]);
+
+        traverseUntilEnd(board, x, y, x - 1, y - 1, -1, -1, ownColor);
+
+        traverseUntilEnd(board, x, y, x + 1, y - 1, 1, -1, ownColor);
+
+        traverseUntilEnd(board, x, y, x - 1, y + 1, -1, 1, ownColor);
+
+        traverseUntilEnd(board, x, y, x + 1, y + 1, 1, 1, ownColor);
+    }
+
+    private void getLegalQueenMoves(Piece[][] board, int x, int y) {
+
+        getLegalRookMoves(board, x, y);
+        getLegalBishopMoves(board, x, y);
+    }
+
+    private void getLegalKingMoves(Piece[][] board, int x, int y) {
+
+        Piece ownColor = getColor(board[y][x]);
+
+        for (int y2 = y - 1; y2 <= y + 1; y2++) {
+            for (int x2 = x - 1; x2 <= x + 1; x2++) {
+                if (x2 >= size || x2 < min || y2 >= size || y2 < min) {
+                    continue;
+                }
+                Piece destinationTileColor = getColor(board[y2][x2]);
+                if (destinationTileColor == getOppositeColor(ownColor) || destinationTileColor == Piece.EMPTY) {
+                    Piece[][] move = moveCharacter(board, x, y, x2, y2);
+                    legalMoves.add(move);
+                }
+            }
+        }
+    }
+
+    private void traverseUntilEnd(Piece[][] board, int startX, int startY, int x, int y, int xSteps, int ySteps, Piece ownColor) {
+        if (x >= size || x < min || y >= size || y < min) {
+            return;
+        }
+
+        Piece tileColor = getColor(board[y][x]);
+
+        if (tileColor == ownColor) {
+            return;
+        }
+
+        if (tileColor == getOppositeColor(ownColor)) {
+            Piece[][] attack = moveCharacter(board, startX, startY, x, y);
+            legalMoves.add(attack);
+            return;
+        }
+
+        Piece[][] moveToThisTile = moveCharacter(board, startX, startY, x, y);
+        legalMoves.add(moveToThisTile);
+
+        traverseUntilEnd(board, startX, startY, x + xSteps, y + ySteps, xSteps, ySteps, ownColor);
+    }
+
+    private Piece[][] moveCharacter(Piece[][] board, int x1, int y1, int x2, int y2) {
+
+        Piece[][] boardToBeReturned = new Piece[8][8];
+
+        for (int i = 0; i < size; i++) {
+            for (int j = 0; j < size; j++) {
+                boardToBeReturned[i][j] = board[i][j];
+            }
+        }
+
+        Piece pieceAtCurrentLocation = board[y1][x1];
+        boardToBeReturned[y2][x2] = pieceAtCurrentLocation;
+        boardToBeReturned[y1][x1] = Piece.EMPTY;
+
+        return boardToBeReturned;
+    }
+
+    public Piece getColor(Piece piece) {
+        if (piece.toString().charAt(0) == 'B') {
+            return Piece.BLACK;
+        }
+        if (piece.toString().charAt(0) == 'W') {
+            return Piece.WHITE;
+        }
+        return Piece.EMPTY;
+
+    }
+
+    public Piece getOppositeColor(Piece piece) {
+        if (getColor(piece) == Piece.WHITE) {
+            return Piece.BLACK;
+        }
+        if (getColor(piece) == Piece.BLACK) {
+            return Piece.WHITE;
+        }
+        return Piece.EMPTY;
+    }
 
 }
