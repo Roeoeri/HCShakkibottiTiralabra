4c92eab1c869f7cd6e84e8e12dcd895c6f044ed9
Roeoeri <tomi.haapakoski@helsinki.fi>
2020-02-29 00:22:42 +0200

    lisätty suorituskykytestausta ja testit siirtojen parseamiselle

--- src/test/java/chess/bot/TiraBotTest.java
+++ src/test/java/chess/bot/TiraBotTest.java
@@ -2,6 +2,8 @@ package chess.bot;
 
 import static org.junit.Assert.*;
 
+import java.util.Random;
+
 import org.junit.After;
 import org.junit.AfterClass;
 import org.junit.Before;
@@ -13,6 +15,8 @@ import chess.rules.Piece;
 public class TiraBotTest {
 	
 	private TiraBot bot;
+	private Piece[] pieceCollection; 
+	private Random rng;
 
 	@BeforeClass
 	public static void setUpBeforeClass() throws Exception {
@@ -25,6 +29,11 @@ public class TiraBotTest {
 	@Before
 	public void setUp() throws Exception {
 		bot = new TiraBot();
+		pieceCollection = new Piece[] {Piece.BKING,Piece.WKING,Piece.BQUEEN,Piece.WQUEEN,
+			   Piece.BBISHOP,Piece.WBISHOP,Piece.BKNIGHT,Piece.WKNIGHT,
+			   Piece.BROOK,Piece.WROOK,Piece.BPAWN,Piece.WPAWN};
+        
+       rng = new Random();
 	}
 
 	@After
@@ -55,4 +64,71 @@ public class TiraBotTest {
        assertEquals(bKingGetsEaten,true);
             
 	}
+	
+	@Test
+	public void botParsesMovesCorrectly() {
+		
+		Piece[][] startBoard = new Piece[][]{
+            {Piece.EMPTY, Piece.EMPTY, Piece.EMPTY, Piece.EMPTY, Piece.EMPTY, Piece.EMPTY, Piece.EMPTY, Piece.EMPTY},
+            {Piece.EMPTY, Piece.EMPTY, Piece.EMPTY, Piece.EMPTY, Piece.EMPTY, Piece.EMPTY, Piece.EMPTY, Piece.EMPTY},
+            {Piece.EMPTY, Piece.EMPTY, Piece.EMPTY, Piece.EMPTY, Piece.EMPTY, Piece.EMPTY, Piece.EMPTY, Piece.EMPTY},
+            {Piece.EMPTY, Piece.EMPTY, Piece.WPAWN, Piece.WKING, Piece.EMPTY, Piece.EMPTY, Piece.EMPTY, Piece.EMPTY},
+            {Piece.EMPTY, Piece.BKING, Piece.EMPTY, Piece.BPAWN, Piece.BPAWN, Piece.EMPTY, Piece.EMPTY, Piece.EMPTY},
+            {Piece.EMPTY, Piece.EMPTY, Piece.EMPTY, Piece.EMPTY, Piece.EMPTY, Piece.EMPTY, Piece.EMPTY, Piece.EMPTY},
+            {Piece.EMPTY, Piece.EMPTY, Piece.EMPTY, Piece.EMPTY, Piece.EMPTY, Piece.EMPTY, Piece.EMPTY, Piece.EMPTY},
+            {Piece.EMPTY, Piece.EMPTY, Piece.EMPTY, Piece.EMPTY, Piece.EMPTY, Piece.EMPTY, Piece.EMPTY, Piece.EMPTY},};
+            
+        Piece[][] endBoard = new Piece[][]{
+                {Piece.EMPTY, Piece.EMPTY, Piece.EMPTY, Piece.EMPTY, Piece.EMPTY, Piece.EMPTY, Piece.EMPTY, Piece.EMPTY},
+                {Piece.EMPTY, Piece.EMPTY, Piece.EMPTY, Piece.EMPTY, Piece.EMPTY, Piece.EMPTY, Piece.EMPTY, Piece.EMPTY},
+                {Piece.EMPTY, Piece.EMPTY, Piece.EMPTY, Piece.EMPTY, Piece.EMPTY, Piece.EMPTY, Piece.EMPTY, Piece.EMPTY},
+                {Piece.EMPTY, Piece.EMPTY, Piece.WPAWN, Piece.WKING, Piece.EMPTY, Piece.EMPTY, Piece.EMPTY, Piece.EMPTY},
+                {Piece.EMPTY, Piece.EMPTY, Piece.EMPTY, Piece.BPAWN, Piece.BPAWN, Piece.EMPTY, Piece.EMPTY, Piece.EMPTY},
+                {Piece.EMPTY, Piece.BKING, Piece.EMPTY, Piece.EMPTY, Piece.EMPTY, Piece.EMPTY, Piece.EMPTY, Piece.EMPTY},
+                {Piece.EMPTY, Piece.EMPTY, Piece.EMPTY, Piece.EMPTY, Piece.EMPTY, Piece.EMPTY, Piece.EMPTY, Piece.EMPTY},
+                {Piece.EMPTY, Piece.EMPTY, Piece.EMPTY, Piece.EMPTY, Piece.EMPTY, Piece.EMPTY, Piece.EMPTY, Piece.EMPTY},};
+                
+        
+        String UCIMove = bot.parseMove(startBoard, endBoard);
+        
+        assertEquals(UCIMove,"b5b6");	
+	}
+	
+	@Test
+	public void botGeneratesPlayInAReasonableAmountOfTime() {
+		
+		for(int i = 0; i< 100; i++) {
+			long startTime = System.nanoTime();
+			bot.getBestBlackMove(generateRandomTestBoard());
+			long endTime = System.nanoTime();
+			long duration = (endTime - startTime)/1000000000;
+			assertTrue(duration < 30);
+		}
+
+	}
+	
+	private Piece[][] generateRandomTestBoard() {
+		
+		Piece[][] testBoard = new Piece[][]{
+            {Piece.EMPTY, Piece.EMPTY, Piece.EMPTY, Piece.EMPTY, Piece.EMPTY, Piece.EMPTY, Piece.EMPTY, Piece.EMPTY},
+            {Piece.EMPTY, Piece.EMPTY, Piece.EMPTY, Piece.EMPTY, Piece.EMPTY, Piece.EMPTY, Piece.EMPTY, Piece.EMPTY},
+            {Piece.EMPTY, Piece.EMPTY, Piece.EMPTY, Piece.EMPTY, Piece.EMPTY, Piece.EMPTY, Piece.EMPTY, Piece.EMPTY},
+            {Piece.EMPTY, Piece.EMPTY, Piece.EMPTY, Piece.EMPTY, Piece.EMPTY, Piece.EMPTY, Piece.EMPTY, Piece.EMPTY},
+            {Piece.EMPTY, Piece.EMPTY, Piece.EMPTY, Piece.EMPTY, Piece.EMPTY, Piece.EMPTY, Piece.EMPTY, Piece.EMPTY},
+            {Piece.EMPTY, Piece.EMPTY, Piece.EMPTY, Piece.EMPTY, Piece.EMPTY, Piece.EMPTY, Piece.EMPTY, Piece.EMPTY},
+            {Piece.EMPTY, Piece.EMPTY, Piece.EMPTY, Piece.EMPTY, Piece.EMPTY, Piece.EMPTY, Piece.EMPTY, Piece.EMPTY},
+            {Piece.EMPTY, Piece.EMPTY, Piece.EMPTY, Piece.EMPTY, Piece.EMPTY, Piece.EMPTY, Piece.EMPTY, Piece.EMPTY},};
+		
+			int amountOfPieces = rng.nextInt(16);
+			for(int i = 0; i< amountOfPieces; i++) {
+				int x = rng.nextInt(8);
+				int y = rng.nextInt(8);
+				Piece randomPiece = pieceCollection[rng.nextInt(pieceCollection.length)];
+				testBoard[y][x] = randomPiece;
+			}
+			
+			return testBoard;
+	}
+		
+		
 }
